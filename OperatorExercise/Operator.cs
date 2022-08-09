362eed5f8df9244b784453eea2eeb21aaa9ba058
Yun Jang <jang7j@yahoo.com>
2022-08-09 14:28:22 -0500

    initial commit

---
+++ OperatorExercise/Operator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    public class Operator
+    {
+
+    //Exercise 1
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Modulo(int a, int b)
+        {
+            return a % b;
+        }
+
+       
+
+    
+
+        
+    }
+
+}
+
+
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OperatorExercise;
+
+//Exercise 1
+
+int quotient = Operator.Divide(17, 4);
+int modulo = Operator.Modulo(17, 4);
+
+
+Console.WriteLine($"17/4 is {quotient} remainder {modulo}");
+
+
+
+
+
+
+
